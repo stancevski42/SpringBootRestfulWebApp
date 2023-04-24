d4d4a4877cbc2ed4243e478c31ba0b90877ee406
Veljko Potkonjak <10220veljkopotkonjak@etsntesla.edu.rs>
2023-04-24 11:35:44 +0200

    save

---
+++ src/main/java/org/etsntesla/it/repositories/EmocijeRepository.java
@@ -0,0 +1,10 @@
+package org.etsntesla.it.repositories;
+
+import org.etsntesla.it.models.Emocije;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmocijeRepository extends JpaRepository<Emocije,Integer>{
+
+}
